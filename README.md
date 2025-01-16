fa12c12007a157d4a73beb274de866efdf2c2ff1
Tyler Allen <tyler.allen@hashicorp.com>
2025-01-16 11:23:40 +0000

    Adding in generating JWT's

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # vault-jwt-testing
+
+## Create keys
+```
+ssh-keygen -t rsa-sha2-256 -b 2048 -f jwt_key -m pem
+ssh-keygen -f jwt_key -y -m pem > jwt_key.pem.pub
+```
+
+## Configure Vault
+```bash
+vault auth enable jwt
+vault write auth/jwt/config jwt_validation_pubkeys=@jwtRS256.key.pub
+```
--- jwt-generator/app.js
+++ jwt-generator/app.js
@@ -1,7 +1,20 @@
-var jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
+const fs = require('fs');
 
-var token = jwt.sign({
-    exp: Math.floor(Date.now() / 1000) + (60 * 60), //1 hour
-  }, 'secret');
+var privateKey = fs.readFileSync('../jwt_key');
+var publicKey = fs.readFileSync('../jwt_key.pub');
+
+var token = jwt.sign({ 
+    foo: 'bar' 
+}, privateKey, { algorithm: 'RS256' });
 
 console.log(token)
+
+jwt.verify(token, privateKey, { algorithms: ['RS256'] }, function(err, decoded) {
+    if (err){
+        console.log("ERROR: " + err)
+    }else{
+        console.log("Valid: ")
+        console.log(decoded)
+    }
+  });
