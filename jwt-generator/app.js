fb8789ec2e264fb052e750987637dfbe2af7ae57
Tyler Allen <tyler.allen@hashicorp.com>
2025-01-16 11:59:17 +0000

    Working on node JWT config

--- jwt-generator/app.js
+++ jwt-generator/app.js
@@ -7,10 +7,44 @@ var vaultOptions = {
     token: process.env.VAULT_TOKEN
 };
 
-var vault = require("node-vault")(options);
+var vault = require("node-vault")(vaultOptions);
 
 var privateKey = fs.readFileSync('../jwt_key');
 var publicKey = fs.readFileSync('../jwt_key.pub');
+
+/*
+vault auth enable jwt
+vault write auth/jwt/config jwt_validation_pubkeys=@jwt_key.pub
+vault write auth/jwt/role/demo-role role_type="jwt" user_claim="sub" bound_subject="vault"
+*/
+async function configVault(){
+    console.log("Enable JWT")
+    await vault.enableAuth('jwt');
+
+    console.log("Config JWT")
+    await vault.write('auth/jwt/config', {
+        jwt_validation_pubkeys: publicKey
+    })
+    
+    console.log("Create demo-role")
+    await vault.write('auth/jwt/role/demo-role', {
+        role_type: "jwt",
+        user_claim: "sub",
+        bound_subject: "vault"
+    })
+}
+
+/*
+vault write auth/jwt/login role=demo-role jwt=
+*/
+async function loginVaultJWT(token){
+    var login = await vault.write('auth/jwt/login', {
+        role: 'demo-role',
+        jwt: token
+    })
+    console.log(login)
+}
+
 
 async function generateJWT() {
     var token = jwt.sign({
@@ -34,6 +68,9 @@ async function verifyJwt(token) {
 async function main() {
     var token = await generateJWT()
     verifyJwt(token)
+
+    await configVault()
+    await loginVaultJWT(token)
 }
 
 main();
